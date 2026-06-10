5c299c49f648a348db5ea441415f582b0e6c74e5
Chichex-ag <anthony.2004giler@hotmail.com>
2026-06-10 08:05:15 -0500

    actualizacion de admin

---
+++ Admin.py
@@ -0,0 +1,21 @@
+class Administrador(Usuario, GestionReportes):
+    def __init__(self, id_usuario, cedula, nombres, apellidos, correo, telefono, estado, contraseña, cargo):
+        super().__init__(id_usuario, cedula, nombres, apellidos, correo, telefono, estado, contraseña)
+        self._cargo = cargo
+
+    @property
+    def cargo(self):
+        return self._cargo
+
+    @cargo.setter
+    def cargo(self, valor):
+        self._cargo = valor
+
+    def gestionar_usuarios(self):
+        pass
+
+    def configurar_parametros(self):
+        pass
+
+    def mostrar_rol(self):
+        return "Administrador"
